a94f6b24d97a7bcc3d7110bf089132549a1522e8
ThalKod <tmarcelin@thal.tech>
2017-12-01 11:39:05 -0500

    Refractoring...

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.post("/login", passport.authenticate("local",{
 }), function(req, res){
 });
 
-app.get("/logout", function(req, req){
+app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/");
 });
